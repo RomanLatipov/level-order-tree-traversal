d85f3f5a45c8f249a7694512b2511f2b2736a936
RomanLatipov <romanlatipov@gmail.com>
2024-07-14 20:59:50 -0400

    complete level-roder-tree-treversal lab

--- javascript/tree_traversal_bfs.js
+++ javascript/tree_traversal_bfs.js
@@ -5,20 +5,60 @@ class Node {
     this.right = right;
   }
 }
+function concatNodes(root) {    
+  let left = [];
+  let right = [];
+  if (root.left)
+    left = root.left;
+  if (root.right)
+    right = root.right;
+  return [left, right]
+}
 
 function levelOrderTraversal(root) {
-  // type your code here
+  if (root === null) return [];
+  const result = [[root.value]];
+  if (root.left || root.right) {
+    let nodes = [root.left, root.right];
+    console.log(nodes)
+  
+    let run = true;
+
+    while (run) {
+      const arr1 = [];
+      const arr2 = [];
+      for (let i=0; i<nodes.length; i++) {
+        if(nodes[i] !== null && nodes[i].length !== 0) {
+          arr1.push(nodes[i].value);
+          arr2.push(concatNodes(nodes[i]));
+        }
+      }
+      if (arr1.length === 0) {
+        run = false;
+        break;
+      }
+      result.push(arr1);
+      nodes = arr2.flat();
+    }
+  }
+  
+  return result.flat();
 }
 
 if (require.main === module) {
   let root = new Node(1, new Node(2), new Node(3));
+                  // 1
+                // 2   3
+
   // add your own tests in here
   console.log("Expecting: [[1], [2, 3]]");
   console.log(levelOrderTraversal(root));
 
-  console.log("");
-
+  // console.log("");
   root = new Node(10, new Node(20, new Node(9), new Node(22)), new Node(30));
+  //                       // 10
+  //                     //20    30
+  //                   //9  22
 
   console.log("Expecting: [[10], [20, 30], [9, 22]]");
   console.log(levelOrderTraversal(root));
